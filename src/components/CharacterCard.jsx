aa74c47707ee1cbb4ae15f5604570c0eb8e60479
ElliotBellaicheLarue <Elliot.BELLAICHE--LARUE@etu.uca.fr>
2026-03-23 13:31:13 +0100

    pret a regrouper

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,9 +2,8 @@ export default function CharacterCard({ character, onClick }) {
   return (
     <div
       onClick={() => onClick(character)}
-      style={{ border: "1px solid #ccc", padding: 10, cursor: "pointer" }}
     >
-      <img src={character.image} alt="" width="100" />
+      <img src={character.image} alt=""/>
       <h3>{character.name}</h3>
       <p>{character.characterClass?.name}</p>
       <p>{character.race?.name}</p>
